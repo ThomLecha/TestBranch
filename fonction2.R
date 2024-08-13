f051971dfb590c607fb824718bdad7516b8e5e70
Thomas Lechalupe <thomas.lechalupe@aviation-civile.gouv.fr>
2024-08-13 10:25:19 +0200

    f2

---
+++ fonction2.R
@@ -0,0 +1,19 @@
+f1 <- function(x,y,z,w){
+  # Additionne 4 nombres
+  return(x+y+z+w)
+}
+
+f2 <- function(x,y,z){
+  # Multiplie 3 nombres
+  return(x*y*z)
+}
+
+f3 <- function(x,y,z){
+  # Divise 3 nombres
+  return(x/y/z)
+}
+
+f4 <- function(x,y){
+  # Soustrais 2 nombres
+  return(x-y-x+y)
+}
